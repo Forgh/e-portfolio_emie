5ecc922c3779bbb7638008ba7d3921cc6d48a8fa
Forgh <mickael.forgh@gmail.com>
2014-07-02 19:43:32 +0200

    First modifications - adapting
    First modifications in order to adapt the website to a more conventional
    portfolio.
    
    - Installations folder deleted.
    - Renamed Dessins folder into works
    - Modified /index.php to redirect only to works folder
    - Renamed works/travaux.php into works/index.php

--- Installations/jquery/script_hoverGoToOther.js
+++
@@ -1,7 +0,0 @@
-function hoverGoToOther (image) {
-	$(image).attr("src", "Dessins/design-ressources/gotohover.png");
-}
-
-function outGoToOther (image) {
-	$(image).attr("src", "Dessins/design-ressources/gotoother.png");
-}
--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
    <head>
-       <title>Stéphane Protic</title>
-	   <meta name="description" content="Site web de Stéphane Protic, artiste plasticien."/>
-       <meta name="keywords" content="Stephane,Stéphane,STEPHANE,Protic,PROTIC,protic,stephane,artiste,Artiste,plasticien,Plasticien"/>
+       <title>Emie Vilar</title>
+	   <meta name="description" content="Site Web d'Emie Vilar, photographe."/>
+       <meta name="keywords" content="Emie,emie,EMIE,Vilar,VILAR,vilar,photographe,photographie,photo"/>
        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-	   <link rel="icon" type="image/png" href="Design-ressources/favicon32.png" />
+	   <link rel="icon" type="image/png" href="design-ressources/favicon32.png" />
 	   <meta name="google-site-verification" content="uRfeboJ3bGEFtuE6yaJ9mWvLPesPSaAq7PPC2QAOygo" />
-	   <link rel="stylesheet" media="screen" type="text/css" href="css/design-stf.css" />
+	   <link rel="stylesheet" media="screen" type="text/css" href="css/design-emie.css" />
        		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 	   <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-		<script src="jquery/scriptFadeInHomePage.js"></script>
 		
 	   
    </head>
    <body>
 		<div id="welcomebox" >
-			<a href="Dessins/travaux.php"><div id="linkDessins" style="display:none;"></div> </a>
-			<img src="design-ressources/welcome.png" id="welcome" alt="welcome" />
-			<a href="Installations/travaux.php"><div id="linkInstallations" style="display:none;"></div> </a>
+			<a href="works/index.php">
+				<img src="design-ressources/welcome.png" id="welcome" alt="welcome" />
+			</a>
 		</div>
 <script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
