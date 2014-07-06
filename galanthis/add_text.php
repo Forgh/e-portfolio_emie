620846fff1d05adc2ea039eb10d6e811447d5009
Forgh <mickael.forgh@gmail.com>
2014-07-06 03:22:50 +0200

    Adapting Galanthis : part 2
    - Add_text form and php functions added.

---
+++ galanthis/add_text.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr" >
+	<head>
+		<title>Galanthis - Modifier Liens</title>
+		<meta charset="UTF-8" />
+		<link rel="icon" type="image/png" href="Design-ressources/favicon32.png" />
+		<link rel="stylesheet" href="css/design-emie.css" />
+	
+	</head>
+	<body>
+	<?php 
+				$page='textes';
+			include("include/entete.php");
+			include("include/menu_galanthis.php");
+	?>
+	<div id="corps_accueil">
+
+		<?php 
+			include("include/link.php");
+			date_default_timezone_set("Europe/Paris"); 
+
+			$titre = $_POST['title'];
+			$texte = $_POST['texte_article'];
+			$date = time();
+			
+			$adding_text = $bdd->prepare('INSERT INTO textes (titre_texte, texte_libre, date_texte) VALUES (:titre_texte, :texte_libre, :date_texte)');
+			$adding_text = $bdd->execute(array(	'titre_texte' => $titre,
+												'texte_libre' => $texte,
+												'date_texte' => $date,
+												));
+		?>
+		<p>Article "<?php echo $titre; ?>" correctement ajouté !</p>
+
+	</div>
+</body>
+	
+	
+</html>
---
+++ galanthis/new_texte.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="fr" >
+	<head>
+		<title>Galanthis - Modifier Liens</title>
+		<meta charset="UTF-8" />
+		<link rel="icon" type="image/png" href="Design-ressources/favicon32.png" />
+		<link rel="stylesheet" href="css/design-stf.css" />
+		<script type="text/javascript" src="tinymce/tinymce.min.js"></script>
+		<script type="text/javascript">
+			tinymce.init({
+				selector: "textarea",
+				browser_spellcheck : true,
+				language : 'fr_FR',
+				height : 500,
+				relative_urls: false,
+				convert_urls: false,
+			    remove_script_host: false,
+				plugins: [
+					"advlist autolink lists link image charmap print preview anchor",
+					"searchreplace visualblocks code fullscreen",
+					"insertdatetime media table contextmenu paste"
+				],
+				toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+			});
+
+		</script>
+
+	</head>
+	<body>
+	<?php 
+				$page='textes';
+			include("include/entete.php");
+			include("include/menu_galanthis.php");
+	?>
+	<div id="corps_accueil">
+
+	
+	<form action="add_text.php" method="post" enctype="multipart/form-data">
+		<fieldset>
+			<p>
+				<input type="text" id="title" name="title">
+				<legend for="title">Titre de l'article : </legend>
+			</p>
+			<p>
+				<textarea name="texte_article">
+					
+				</textarea>
+			</p>
+			
+			<p>
+				<input type="validate" value="Envoyer">
+			</p>
+		</fieldset>
+	<p>
+		<input type="submit" value="Valider" />
+	</p>
+	
+	</form>
+	</div>
+</body>
+	
+	
+</html>
